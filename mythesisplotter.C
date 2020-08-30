7f53418f61724871246aa2ac187a87af8a35df3c
Joaquin E Siado Castaneda <jsiado@huskers.unl.edu>
2020-08-30 00:32:34 -0500

    ch7 and ch2 update

---
+++ mythesisplotter.C
@@ -0,0 +1,37 @@
+// #include <TTree.h>
+// #include <TFile.h>
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TH1F.h>
+// #include <TH2F.h>
+#include <TMath.h>
+#include "Math/LorentzVector.h"
+#include "TLorentzVector.h"
+#include "TVector.h"
+#include "TVector2.h"
+#include "TVector3.h"
+// // Header file for the classes stored in the TTree if any.
+#include <vector>
+#include <fstream>
+#include <iostream>
+// Header file for the classes stored in the TTree if any.
+#include "TClonesArray.h"
+#include "TObject.h"
+#include "TVector3.h"
+
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TStyle.h"
+#include "TF1.h"
+
+
+void myplotterall()
+{
+	
+	TFile* file2 = TFile::Open("hst_RHN_13TeV_2GeV.root");
+	
+	
+	
+	
+}
